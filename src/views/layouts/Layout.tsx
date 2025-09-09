ac98a98f2553ba395d54ef4b5d3eec735db7e9c8
Kevin Durbin <kevindurb@fastmail.com>
2025-09-09 15:06:44 -0600

    basic styling and layout

--- src/views/layouts/Layout.tsx
+++ src/views/layouts/Layout.tsx
@@ -1,7 +1,19 @@
 import type { FC } from "hono/jsx";
+import { Nav } from "../components/Nav";
 
 export const Layout: FC = ({ children }) => (
 	<html lang="en">
-		<body>{children}</body>
+		<head>
+			<link
+				rel="stylesheet"
+				href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+			/>
+		</head>
+		<body>
+			<main class="container">
+				<Nav />
+				{children}
+			</main>
+		</body>
 	</html>
 );
